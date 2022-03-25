d8b78a02fb3521182818d63418663f86bf1fb0df
Nick <spbkolia@gmail.com>
2022-03-25 14:42:24 +0300

    Initial Commit

---
+++ Mobile Up Gallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mobile Up Gallery
+//
+//  Created by Чаусов Николай on 25.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
